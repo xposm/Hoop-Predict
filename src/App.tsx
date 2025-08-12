8bcac71f2cfc155ad14d28402d2fb5aad9146e2e
A_A <81513060tt"gmail.com>
2025-08-12 14:37:05 +1200

    Combobox

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Page from './dashboard/page'
+import { ComboboxDemo } from './dashboard/pagecontent/content'
+
+function App() {
+  return (
+      <div>
+      <Page />
+      
+      
+      </div>
+    
+  )
+}
+
+export default App
---
+++ src/dashboard/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { ComboboxDemo } from "./pagecontent/content" // <-- Import your ComboboxForm
+
+export default function Page() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+           
+          </div>
+        </header>
+        
+        </SidebarInset>
+      
+      <ComboboxDemo /> {/* Centered ComboboxForm */}
+    </SidebarProvider>
+  )
+}
